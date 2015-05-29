b6454d4b48945fdab14b1598b7b7c7171655ada5
maximus <maxim.chernyatevich@gmail.com>
2015-05-29 10:27:17 +0300

    Change migration.

--- playlist_miner_project/apps/core/migrations/0001_initial.py
+++ playlist_miner_project/apps/core/migrations/0001_initial.py
@@ -11,67 +11,52 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Airplay',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(null=True, verbose_name='Date of airplay', blank=True)),
+            ],
+            options={
+                'ordering': ['-date'],
+                'verbose_name': 'Airplay',
+                'verbose_name_plural': 'Airplays',
+            },
+        ),
+        migrations.CreateModel(
             name='Radiostation',
             fields=[
-                ('id', models.AutoField(verbose_name='ID',
-                                        serialize=False, auto_created=True,
-                                        primary_key=True)),
-                ('name', models.CharField(max_length=255,
-                                          verbose_name='Name')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
                 ('url', models.CharField(max_length=255, verbose_name='URL')),
-                ('data_url', models.CharField(max_length=500,
-                                              verbose_name='Data URL')),
-                ('period', models.PositiveIntegerField(
-                    default=7,
-                    verbose_name='Monitoring period (days)')),
-
+                ('data_url', models.CharField(max_length=500, verbose_name='Data URL')),
+                ('period', models.PositiveIntegerField(default=7, verbose_name='Monitoring period (days)')),
             ],
             options={
                 'verbose_name': 'Radiostation',
                 'verbose_name_plural': 'Radiostations',
             },
         ),
-
-        migrations.CreateModel(
-            name='Airplay',
-            fields=[
-                ('id', models.AutoField(
-                    verbose_name='ID',
-                    serialize=False,
-                    auto_created=True,
-                    primary_key=True)),
-                ('radiostation', models.ForeignKey(to='core.Radiostation')),
-                ('song', models.ForeignKey(to='core.Song')),
-                ('date', models.DateTimeField(
-                    null=True,
-                    verbose_name='Date of airplay',
-                    blank=True)),
-            ],
-            options={
-                'verbose_name': 'Airplay',
-                'verbose_name_plural': 'Airplays',
-            },
-        ),
-
         migrations.CreateModel(
             name='Song',
             fields=[
-                ('id', models.AutoField(
-                    verbose_name='ID',
-                    serialize=False,
-                    auto_created=True,
-                    primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='Song title')),
+                ('artist', models.CharField(max_length=255, verbose_name='Artist')),
                 ('radiostation', models.ForeignKey(to='core.Radiostation')),
-                ('title', models.CharField(
-                    max_length=255,
-                    verbose_name='Song title')),
-                ('artist', models.CharField(
-                    max_length=255,
-                    verbose_name='Artist')),
             ],
             options={
                 'verbose_name': 'Song',
                 'verbose_name_plural': 'Songs',
             },
+        ),
+        migrations.AddField(
+            model_name='airplay',
+            name='radiostation',
+            field=models.ForeignKey(to='core.Radiostation'),
+        ),
+        migrations.AddField(
+            model_name='airplay',
+            name='song',
+            field=models.ForeignKey(to='core.Song'),
         ),
     ]
